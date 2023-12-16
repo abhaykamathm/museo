7c7240888a683baee892d49f1a85584b7f28a693
bhahirathi262@gmail.com <102673355+Bhahirathy@users.noreply.github.com>
2023-12-16 20:07:37 +0530

    Added Verification and Card Added page

---
+++ src/pages/Login/VerificationSuccessPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VerificationSuccess from "../../components/Login/VerificationSuccess";
+
+function VerificationSuccessPage() {
+  return (
+    <>
+      <VerificationSuccess />
+    </>
+  );
+}
+
+export default VerificationSuccessPage;
